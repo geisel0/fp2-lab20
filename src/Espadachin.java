479797d0ca789e405ba2b462194bd1a4b670fb01
geisel0 <gpachecome@unsa.edu.pe>
2024-12-07 21:47:47 -0500

    espadachin mejorado

--- src/Espadachin.java
+++ src/Espadachin.java
@@ -1,24 +1,34 @@
 public class Espadachin extends Soldado {
     private int longitudEspada;
-    private static int contador=0;
 
     public Espadachin() {
-        super();
-        setNombre("Espadachin" + contador);
-        setNivelAtaque(3);
-        setNivelDefensa(2);
-        longitudEspada = 100; // Longitud en centímetros
+        super(3, 4);
+        this.longitudEspada = new Random().nextInt(50) + 50;
+        this.nombre = "Espadachin" + contador;
     }
 
-    public void muroDeEscudos() {
-        System.out.println(getNombre() + " crea un muro de escudos.");
+    @Override
+    public void accionEspecial() {
+        this.nivelDefensa += 1;
+    }
+}
+
+// Clase Arquero
+public class Arquero extends Soldado {
+    private int flechas;
+
+    public Arquero() {
+        super(1, 3);
+        this.flechas = 10;
+        this.nombre = "Arquero" + contador;
     }
 
-    public int getLongitudEspada() {
-        return longitudEspada;
+    public void dispararFlecha() {
+        if (flechas > 0) flechas--;
     }
 
-    public void setLongitudEspada(int longitudEspada) {
-        this.longitudEspada = longitudEspada;
+    @Override
+    public void accionEspecial() {
+        this.dispararFlecha();
     }
 }
